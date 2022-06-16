9a0b7f44f656971c0132e10ee28d2c83e846a7ea
Brian Wangora <brianwangora@gmail.com>
2022-06-16 09:08:55 +0300

    liker built

--- main.js
+++ main.js
@@ -3,6 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector('#modal')
+modal.setAttribute("class", "hidden")
+
+const glyphStates = {
+  '♡': FULL_HEART,
+  '♥': EMPTY_HEART
+}
+
+const colorStates = {
+  "red": "",
+  "": "red"
+}
+
+const articleLikes = document.querySelectorAll(".like-glyph");
+
+function likeCallBack(event) {
+  const heart = event.target
+  mimicServerCall() 
+    .then(function (serverMessage) {
+      alert("You have notified the server!")
+        alert(serverMessage)
+      heart.innerText = glyphStates[heart.innerText]
+      heart.style.color = colorStates[heart.style.color]  
+    })
+    .catch(function(error) {
+      modal.removeAttribute("class")
+        alert("Something isn't right!")
+      setTimeout(modal.setAttribute("class", "hidden") , 3000)
+    })
+}
+
+for (const glyph of articleLikes) {
+glyph.addEventListener("click", likeCallBack)
+}
+
+
 
 
 
